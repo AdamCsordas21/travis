8de64de19d3e04d20264a7aa08fa77630b7e0173
AdamCsordas21 <adamcsordas@hotmail.com>
2020-06-16 20:38:35 +0100

    add jest
    add node_modules to gitignore
    initialise npm package
    export greetings function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function greetings () {
   return 'say hello'
 }
+
+module.exports = greetings;
--- index.spec.js
+++ index.spec.js
@@ -1,3 +1,5 @@
+const greetings = require('./')
+
 describe('greetings', function() {
   it('says hello', function() {
     expect(greetings()).toEqual('say hello')
